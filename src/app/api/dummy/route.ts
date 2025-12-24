d8912c21a6f9cc488adca8a1c571058d4ef8beb2
jatindecoded <jatinbhasin2012@gmail.com>
2025-12-25 02:26:39 +0530

    fix

--- src/app/api/dummy/route.ts
+++ src/app/api/dummy/route.ts
@@ -1,16 +1,17 @@
 
+import { getCloudflareContext } from '@opennextjs/cloudflare';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
   // Fetch data from a real-world dummy API
   try {
-    const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-    if (!res.ok) {
-      return NextResponse.json({ error: 'Failed to fetch data' }, { status: 500 });
-    }
-    const data = await res.json();
-    return NextResponse.json(data);
+    const {env} = await getCloudflareContext({async: true});
+    // Query all table names from sqlite_master
+    const tablesResult = await env.DB.prepare("SELECT * from test").all();
+    console.log('Tables in DB:', tablesResult);
+    return NextResponse.json({ tables: tablesResult });
   } catch (error) {
+    console.log('Error fetching data:', error);
     return NextResponse.json({ error: 'Error fetching data', message: error as string }, { status: 500 });
   }
 }
